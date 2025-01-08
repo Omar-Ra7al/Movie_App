94c119cfa62248cdae3bc733096a9e39bfdb0721
OMAR-RAHAL <omar.ra7al@icloud.com>
2025-01-08 05:01:27 +0200

    first commit

--- app/components/client/ToggleMode.tsx
+++ app/components/client/ToggleMode.tsx
@@ -15,12 +15,12 @@ const ThemeToggleButton = () => {
   };
 
   return (
-    <li>
+    <li className="w-10 h-10">
       <button
         aria-label={"toggle_mode"}
         onClick={toggleTheme}
         className="flex items-center justify-center rounded-2xl 
-                border-2 border-secondary w-10 h-10 cursor-pointer
+                border-2 border-secondary w-full h-full cursor-pointer
                 hover:border-active transition duration-300 mt-3 p-3">
         {darkMode ? (
           <IoMdSunny className="text-yellow-500 text-md" size={20} />
